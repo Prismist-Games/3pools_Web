b2b9b0beb3cb424dbe8232a84cc1a9d9054d6816
BlaerZhang <zhangyueyang0022@126.com>
2026-04-09 17:29:10 -0400

    fix: hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
@@ -9,9 +9,9 @@ import { LanguageProvider } from './contexts/LanguageContext';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <LanguageProvider>
-            <BrowserRouter basename="/3pools_Web/">
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </LanguageProvider>
     </React.StrictMode>,
 )
